0369a42f465ef594accc181bfff61de953461203
Islam Ardaraghmeh <islamaldaraghmeh@gmail.com>
2025-11-18 00:05:31 +0400

    Deply files added

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // Because the site will be served at /Matrix_Path/
-  base: '/Matrix_Path/',
+  base: '/Matrix_Project/',
 })
